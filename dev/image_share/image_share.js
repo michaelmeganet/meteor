fdbcd66bd5a7cfb21fb6bf10eca99e1b22c3bac8
cct3000 <cct3000@gmail.com>
2016-06-05 14:33:46 +0800

    after lesson 4 of 7 Better start up script, removing items from a collection

---
+++ dev/image_share/image_share.js
@@ -0,0 +1,36 @@
+Images = new Mongo.Collection("images");
+console.log(Images.find().count());
+
+if (Meteor.isClient){
+
+	console.log("I am the client")
+	
+	
+	//Template.images.helpers({images:img_data});
+
+	Template.images.helpers({images:Images.find()});
+	Template.images.events({
+		'click .js-image':function(event){
+			console.log(event);
+			$(event.target).css("width", "50px");
+		},
+
+
+		'click .js-del-image':function(event){
+			var image_id = this._id;
+			console.log(image_id);
+			$("#"+image_id).hide('slow', function(){
+				Images.remove({"_id":image_id});
+			})
+			
+		}
+	});
+}
+
+
+if (Meteor.isServer){
+
+	console.log("I am the Server")
+
+}
+	
