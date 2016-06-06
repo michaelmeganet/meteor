0e1f4efdf954ffd477ddf2c3ddc706023e7563ed
cct3000 <cct3000@gmail.com>
2016-06-06 21:14:31 +0800

    after lesson 6 of 7, Impement image adding with a bootstrap modal(part 1)

--- dev/image_share/image_share.js
+++ dev/image_share/image_share.js
@@ -9,7 +9,7 @@ if (Meteor.isClient){
 	//Template.images.helpers({images:img_data});
 
 	Template.images.helpers({images:
-		Images.find({}, {sort:{rating:-1}})
+		Images.find({}, {sort:{creatOn: -1, rating:-1}})
 	});
 	Template.images.events({
 		'click .js-image':function(event){
@@ -35,8 +35,23 @@ if (Meteor.isClient){
 			console.log(image_id);
 			Images.update({_id:image_id}, 
 						  {$set: {rating:rating}});
-		}
+		},
+	});
+	Template.image_add_form.events({
+		'submit .js-add-image':function(event){
+			var img_src, img_alt;
+			img_src = event.target.img_src.value;
+			img_alt = event.target.img_alt.value;
+			console.log("src: "+img_src+" alt:"+img_alt);
 
+				Images.insert({
+				img_src: img_src,
+				img_alt: img_alt,
+				createOn: new Date()
+			});
+
+			return false;
+		},
 
 	});
 }
