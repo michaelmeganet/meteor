026994577daca1f77893aba56ff5a59c39eec729
cct3000 <cct3000@gmail.com>
2016-06-06 13:14:25 +0800

    committed at 6 june 2016 13:14

--- dev/image_share01/image_share.js
+++ dev/image_share01/image_share.js
@@ -1,38 +1,24 @@
+// this is image_share.jhs
+Images = new Mongo.Collection("images");
+
 if (Meteor.isClient){
 
 	console.log("I am the client")
 	
-	var img_data = [
-	{
-		img_src:"kingscout.jpg",
-		img_alt:"King Scout Award at 1988"
-		
-	},
-	{
-		img_src:"Flag.jpg",
-		img_alt:"Falg in papar KK camping"
-		
-	},
-	{
-		img_src:"gang.jpg",
-		img_alt:"18th KK gang in 1983"
-		
-
-	},
-	];
-	Template.images.helpers({images:img_data});
+	//Template.images.helpers({images:img_data});
+	Template.images.helpers({images:Images.find()});
 	Template.images.events({
 		'click .js-image':function(event){
 			console.log(event);
 			$(event.target).css("width", "50px");
+		},
+
+		'click .js-del-image':function(event){
+			var image_id = this._id;
+			console.log(image_id);
 		}
+
 	});
 }
 
 
-if (Meteor.isServer){
-
-	console.log("I am the Server")
-
-}
-	
